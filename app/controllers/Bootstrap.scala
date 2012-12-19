0d1dd57601c15b36bb7aadb80cdf63109b7a1a85
Andy Czerwonka <andy.czerwonka@gmail.com>
2012-12-18 17:30:59 -0700

    bootstrap the angular app via the Play routing mechanism
    - also add bootstrap CSS library
    - removed the old javascript, CSS, images
    
    We're now an AngularJS application talking to a back end powered by Play

---
+++ app/controllers/Bootstrap.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import java.io.File
+
+object Bootstrap extends Controller {
+  
+  def index = Action { request=>
+    Ok.sendFile(content=new File("public/app/index.html"), inline=true).as("text/html")
+}
+  
+}

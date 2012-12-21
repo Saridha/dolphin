9f1b40961b328efb7f89ddd79637c293315ccdba
Andy Czerwonka <andy.czerwonka@gmail.com>
2012-12-21 11:26:50 -0700

    more progress on the main toolbar

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('dolphin', ['dolphin.filters', 'dolphin.services', 'dolphin.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: MyCtrl1});
+    $routeProvider.when('/help', {templateUrl: 'partials/help.html', controller: MyCtrl2});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);

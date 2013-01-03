1d9e704f2b65f06a5143499c92c733302455bedb
Andy Czerwonka <andy.czerwonka@gmail.com>
2013-01-03 15:28:30 -0700

    added the gravatar endpoint and the logout action

--- app/controllers/Authenticator.scala
+++ app/controllers/Authenticator.scala
@@ -1,37 +1,39 @@
 package controllers
 
 import play.api.Play.current
-
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws.WS
 import java.io.File
 import play.api.cache.Cache
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee.Enumerator
 
 object Authenticator extends Controller {
 
   case class User(id: String, email: String)
 
-  def login = Action(parse.json) { implicit request =>
-    def sha1(str: String): String = {
-      def hex(array: Array[Byte]): String = {
-        val sb = new StringBuffer();
-        for (i <- 0 to array.length - 1) {
-          sb.append(Integer.toHexString((array(i) & 0xFF) | 0x100).substring(1, 3));
-        }
-        sb.toString();
+  private[this] def hash(str: String): String = {
+    def hex(array: Array[Byte]): String = {
+      val sb = new StringBuffer();
+      for (i <- 0 to array.length - 1) {
+        sb.append(Integer.toHexString((array(i) & 0xFF) | 0x100).substring(1, 3));
       }
-      import java.util._
-      import java.io._
-      import java.security._
-      val md = MessageDigest.getInstance("SHA1")
-      hex(md.digest(str.getBytes("CP1252")))
+      sb.toString();
     }
+    import java.util._
+    import java.io._
+    import java.security._
+    val md = MessageDigest.getInstance("MD5")
+    hex(md.digest(str.getBytes("CP1252")))
+  }
 
+  def login = Action(parse.json) { implicit request =>
     val id = (request.body \ "id").asOpt[String]
     val email = (request.body \ "email").asOpt[String]
     (id, email) match {
       case (Some(id), Some(email)) => {
-        val key = sha1(id)
+        val key = hash(id)
         val user = User(id, email)
         Cache.set(key, user)
         Ok(s"Login for $id successful").withSession("DOLPHIN_SESSION" -> key)
@@ -40,13 +42,29 @@ object Authenticator extends Controller {
     }
   }
 
+  def logout = Action { implicit request =>
+    session.get("DOLPHIN_SESSION").map { key =>
+      Cache.getAs[User](key) match {
+        case (Some(user)) => Ok.withNewSession.flashing("success" -> s"$user.id is now logged out.")
+        case _ => Unauthorized
+      }
+    }.getOrElse {
+      Unauthorized
+    }
+  }
+
   def gravatar = Action { implicit request =>
     val genericImage = Ok.sendFile(content = new File("public/app/img/generic_user.png"), inline = true).as("image/png")
     session.get("DOLPHIN_SESSION").map { key =>
-      Cache.get(key) match {
+      Cache.getAs[User](key) match {
         case (Some(user)) => {
-          println(s"Hi $user, I see you're already successfully authenticated.")
-          genericImage
+          Async {
+            WS.url("http://www.gravatar.com/avatar/" + hash(user.email) + "?s=32&r=pg&d=mm").withTimeout(5000).get
+              .map(image => Ok(image.ahcResponse.getResponseBodyAsBytes()).as("image/jpeg"))
+              .recover {
+                case error => genericImage
+              }
+          }
         }
         case _ => genericImage
       }
--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -3,15 +3,30 @@
 /* Controllers */
 
 var NavigationCtrl = function($scope, $http, $window) {
-    
+
     $scope.user = {};
 
-    $scope.authenticate = function() {
+    $scope.login = function() {
         $http.post("/api/login", angular.toJson($scope.user)).success(function(data, status) {
             $scope.status = status;
             $scope.data = data;
             $scope.result = data;
             $scope.closeLoginDialog();
+            $window.location.reload();
+            console.log(data);
+        }).error(function(err, status) {
+            $scope.status = status;
+            $scope.data = undefined;
+            $scope.errorMsg = err;
+            console.log(err);
+        });
+    };
+
+    $scope.logout = function() {
+        $http.post("/api/logout", {}).success(function(data, status) {
+            $scope.status = status;
+            $scope.data = data;
+            $scope.result = data;
             $window.location.reload();
             console.log(data);
         }).error(function(err, status) {
@@ -30,7 +45,7 @@ var NavigationCtrl = function($scope, $http, $window) {
     $scope.closeLoginDialog = function() {
         $scope.loginDialog = false;
     };
-    
+
 };
 NavigationCtrl.$inject = [ '$scope', '$http', '$window' ];
 

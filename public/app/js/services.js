69493c2a0790647cfee2872c2241f44a5899e91a
Andy Czerwonka <andy.czerwonka@gmail.com>
2013-01-04 15:50:26 -0700

    refactor the service into more idiomatic javascript

--- public/app/js/services.js
+++ public/app/js/services.js
@@ -6,36 +6,38 @@
 // In this case it is a simple value service.
 var service = angular.module('dolphin.services', []).value('version', '0.1');
 
-service.factory('userService', function($http) {
+service.factory('userService', [ '$http', function($http) {
 
-    var service = {};
+    var isEmpty = function(obj) {
+        return Object.keys(obj).length === 0;
+    };
+
     var currentUser = {};
-
     $http.get("/api/whoami").success(function(data) {
         currentUser = data;
     }).error(function() {
         currentUser = {};
     });
 
-    service.login = function(user, successFunction, errorFunction) {
-        $http.post("/api/login", user).success(function(data, status, headers, config) {
-            successFunction(data, status, headers, config);
-        }).error(function(data, status, headers, config) {
-            errorFunction(data, status, headers, config);
-        });
+    return {
+        name : 'User Service',
+
+        login : function(user, onSuccess, onError) {
+            $http.post("/api/login", user).success(function(data, status, headers, config) {
+                onSuccess(data, status, headers, config);
+            }).error(function(data, status, headers, config) {
+                onError(data, status, headers, config);
+            });
+        },
+
+        currentUser : function() {
+            return currentUser;
+        },
+
+        isLoggedIn : function() {
+            return !isEmpty(currentUser);
+        }
+
     };
 
-    service.currentUser = function() {
-        return currentUser;
-    };
-
-    service.isLoggedIn = function() {
-        return !isEmpty(currentUser);
-    };
-
-    var isEmpty = function(obj) {
-        return Object.keys(obj).length === 0;
-    };
-
-    return service;
-});
+} ]);
